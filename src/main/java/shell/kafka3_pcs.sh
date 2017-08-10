93b0d3edeaf82ceb7d721300bd032f1b56a080d8
liuzy <yjwkhhbj@163.com>
2017-08-10 16:37:43 +0800

    p&c s

--- src/main/java/shell/kafka3_pcs.sh
+++ src/main/java/shell/kafka3_pcs.sh
@@ -9,16 +9,34 @@ echo "P"
 
 $P --threads 1 --batch-size 1000 --message-size 1000
 $P --threads 2 --batch-size 1000 --message-size 1000
+$P --threads 3 --batch-size 1000 --message-size 1000
+$P --threads 4 --batch-size 1000 --message-size 1000
 $P --threads 5 --batch-size 1000 --message-size 1000
+$P --threads 6 --batch-size 1000 --message-size 1000
+$P --threads 7 --batch-size 1000 --message-size 1000
+$P --threads 8 --batch-size 1000 --message-size 1000
+$P --threads 9 --batch-size 1000 --message-size 1000
 $P --threads 10 --batch-size 1000 --message-size 1000
-$P --threads 50 --batch-size 1000 --message-size 1000
-$P --threads 100 --batch-size 1000 --message-size 1000
+$P --threads 11 --batch-size 1000 --message-size 1000
+$P --threads 12 --batch-size 1000 --message-size 1000
+$P --threads 13 --batch-size 1000 --message-size 1000
+$P --threads 14 --batch-size 1000 --message-size 1000
+$P --threads 15 --batch-size 1000 --message-size 1000
 
 echo "C"
 
 $C --threads 1 --batch-size 1000 --message-size 1000
 $C --threads 2 --batch-size 1000 --message-size 1000
+$C --threads 3 --batch-size 1000 --message-size 1000
+$C --threads 4 --batch-size 1000 --message-size 1000
 $C --threads 5 --batch-size 1000 --message-size 1000
+$C --threads 6 --batch-size 1000 --message-size 1000
+$C --threads 7 --batch-size 1000 --message-size 1000
+$C --threads 8 --batch-size 1000 --message-size 1000
+$C --threads 9 --batch-size 1000 --message-size 1000
 $C --threads 10 --batch-size 1000 --message-size 1000
-$C --threads 50 --batch-size 1000 --message-size 1000
-$C --threads 100 --batch-size 1000 --message-size 1000
+$C --threads 11 --batch-size 1000 --message-size 1000
+$C --threads 12 --batch-size 1000 --message-size 1000
+$C --threads 13 --batch-size 1000 --message-size 1000
+$C --threads 14 --batch-size 1000 --message-size 1000
+$C --threads 15 --batch-size 1000 --message-size 1000
